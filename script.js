76d8a153e02e361de90f2beec5ba6c65b31e4ed4
Vishal Shinde <itsvishal2417@gmail.com>
2021-12-11 13:22:58 +0530

    Changed the background

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ const videoElement = document.getElementById('video');
 const startButton = document.getElementById('start-button');
 const button = document.getElementById('button');
 
-
 button.addEventListener("click", async () => {
-
-
-     try {
-         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-         videoElement.srcObject = mediaStream;
-         videoElement.onloadedmetadata = () => {
-             videoElement.play();
-         }
-     } catch (error) {
-         console.log('error', error);
-     }
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('error', error);
+    }
 });
 
-startButton.addEventListener('click' , async() => {
+startButton.addEventListener('click', async () => {
     button.disabled = true;
     await videoElement.requestPictureInPicture();
     button.disabled = false;
